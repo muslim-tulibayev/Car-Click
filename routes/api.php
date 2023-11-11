ef68f8c8ec2065cfb3810c4c8b57c546bed4cb17
unknown <ibn970sina@gmail.com>
2023-11-11 20:58:32 +0500

    fix ap.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix' => '/webhook'], function () {
+    Route::post('/operators', [App\Http\Controllers\BotController\OperatorController::class, 'handle']);
+    Route::post('/dealers', [App\Http\Controllers\BotController\DealerController::class, 'handle']);
+    Route::post('/users', [App\Http\Controllers\BotController\UserController::class, 'handle']);
+});
+
 Route::controller(\App\Http\Controllers\API\AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('logout', 'logout');
