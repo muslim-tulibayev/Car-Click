6abdd4a0d0bfe4b4a64ef5b6778aa8c437468851
temurbek23 <tnuriddinov2003@gmail.com>
2023-11-11 17:45:54 +0500

    fix: API

---
+++ app/Http/Resources/BidResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BidResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'auction_id' => $this->auction_id,
+            'dealer' => $this->dealer,
+            'price' => $this->price,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
